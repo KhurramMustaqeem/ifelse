d3d9681e60d50d0f311732f5718b913939c1a41a
khurrammustaqeem <itzkhurram367@gmail.com>
2022-10-11 01:30:33 +0500

    sss

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ let button = document.querySelector('.input')
 
 function check() {
     if (button.value % 2 == 0) {
-        document.getElementById('output').value = "Even";
+                document.getElementById('output').value = "Even";
         
+    }
+    else if(button.value == " "){
+        document.getElementsByClassName('input').value = "Invalid Number"
+
     }
     else{
         document.getElementById('output').value = "odd";
 
     }
-}
+}` `
